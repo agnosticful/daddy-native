f01ebb13fb757d0efb9cf89dcc9cd554710690af
Kohei Asai <me@axross.io>
2019-02-20 23:23:44 -0800

    Add a font

--- src/Application.tsx
+++ src/Application.tsx
@@ -58,7 +58,8 @@ export default class App extends Component<Props> {
         // ...Ionicons.font,
         // We include SpaceMono because we use it in HomeScreen.js. Feel free
         // to remove this if you are not using it in your app
-        "space-mono": require("../assets/fonts/SpaceMono-Regular.ttf")
+        "space-mono": require("../assets/fonts/SpaceMono-Regular.ttf"),
+        worksans: require("../assets/fonts/WorkSans-Regular.ttf")
       })
     ]);
   };
